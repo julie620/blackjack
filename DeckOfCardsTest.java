18713e9237a2d4bd926dfffa1e1c7071e6adc597
Dylan <wilsondylan626@gmail.com>
2024-10-06 15:42:36 -0700

    Update DeckOfCardsTest.java
    added: myDeckOfCards.intro(); // print instructions

--- DeckOfCardsTest.java
+++ DeckOfCardsTest.java
@@ -6,7 +6,7 @@ public class DeckOfCardsTest {
     public static void main(String[] args) {
         DeckOfCards myDeckOfCards = new DeckOfCards();
         myDeckOfCards.shuffle(); // place Cards to random order
-
+        myDeckOfCards.intro(); // print instructions
         myDeckOfCards.game(); // plays through a single game
 
     } // end of main method
